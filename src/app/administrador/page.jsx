01601ce32ba1108d6aa10ea6f474ebe19bda6399
Jose <jojeda5171@uta.edu.ec>
2024-07-20 14:31:18 -0500

    menu

--- src/app/administrador/page.jsx
+++ src/app/administrador/page.jsx
@@ -1,14 +1,14 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 function Admin() {
     
   
     // Contenido de la página de administrador
     return (
-      <div>
-        <h1>Página de Administrador</h1>
+      <div className='bg-pink-500 w-full h-full'>
+        <Sidebar></Sidebar>
+        <h1 className='text-red-600'>Página de Administrador</h1>
       </div>
     );
   }
