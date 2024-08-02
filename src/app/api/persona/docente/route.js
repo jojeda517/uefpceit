8ad99af49025da5e2aa0ed66434a8b6626228f5c
Jose <jojeda5171@uta.edu.ec>
2024-08-02 16:49:39 -0500

    feat: Mejorar selección automática de ubicación en la página de Datos

--- src/app/api/persona/docente/route.js
+++ src/app/api/persona/docente/route.js
@@ -1,144 +1,131 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 import bcrypt from "bcrypt";
+import path from "path";
+import { writeFile } from "fs/promises";
 
-export async function POST(request, { params }) {
-  const body = await request.json();
+export async function POST(request) {
+  const data = await request.formData();
+
+  // Extraer datos del formulario
+  const body = Object.fromEntries(data.entries());
+  const foto = data.get("foto"); // Asegúrate de que el nombre del campo coincida
 
   // Validar cédula
   if (!validarCedulaEcuatoriana(body.cedula)) {
     return NextResponse.json({ error: "Cédula inválida" }, { status: 400 });
   }
 
-  // Verificar si la cedula ya existe sólo si es un nuevo usuario
-  if (!body.id) {
-    const cedulaExistente = await prisma.pERSONA.findUnique({
-      where: { cedula: body.cedula },
-    });
-    if (cedulaExistente) {
-      return NextResponse.json(
-        { error: "La cédula ya está en uso" },
-        { status: 400 }
-      );
-    }
-  } else {
-    // Verificar si la cedula ya está en uso por otro usuario
-    const cedulaExistente = await prisma.pERSONA.findUnique({
-      where: { cedula: body.cedula },
-    });
-    if (cedulaExistente && cedulaExistente.id != body.id) {
-      // Si la cedula existe y no es el mismo usuario que se está actualizando
-      return NextResponse.json(
-        { error: "La cédula ya está en uso" },
-        { status: 400 }
-      );
-    }
+  // Convertir id a entero si existe
+  const id = body.id ? parseInt(body.id, 10) : null;
+
+  // Verificar existencia de cédula
+  const cedulaExistente = await prisma.pERSONA.findUnique({
+    where: { cedula: body.cedula },
+  });
+  if (cedulaExistente && cedulaExistente.id !== id) {
+    return NextResponse.json(
+      { error: "La cédula ya está en uso" },
+      { status: 400 }
+    );
   }
 
-  // Verificar si el correo ya existe sólo si es un nuevo usuario
-  if (!body.id) {
-    console.log("entro a verificar correo");
-    const correoExistente = await prisma.uSUARIO.findUnique({
-      where: { correo: body.correo },
-    });
-    if (correoExistente) {
-      return NextResponse.json(
-        { error: "El correo ya está en uso" },
-        { status: 400 }
-      );
-    }
-  } else {
-    // Verificar si el correo ya está en uso por otro usuario
-    const correoExistente = await prisma.uSUARIO.findUnique({
-      where: { correo: body.correo },
-    });
-    if (correoExistente && correoExistente.idPersonaPertenece != body.id) {
-      // Si el correo existe y no es el mismo usuario que se está actualizando
-      return NextResponse.json(
-        { error: "El correo ya está en uso" },
-        { status: 400 }
-      );
-    }
+  // Verificar existencia de correo
+  const correoExistente = await prisma.uSUARIO.findUnique({
+    where: { correo: body.correo },
+  });
+  if (correoExistente && correoExistente.idPersonaPertenece !== id) {
+    return NextResponse.json(
+      { error: "El correo ya está en uso" },
+      { status: 400 }
+    );
   }
 
-  // Encriptar contraseña si es nueva o se está actualizando
+  // Encriptar contraseña
   let hashedPassword = body.contrasena;
-  if (!body.id || body.contrasena) {
+  if (!id || body.contrasena) {
     const saltRounds = 10;
     hashedPassword = await bcrypt.hash(body.contrasena, saltRounds);
   }
 
+  // Manejo de la foto
+  let fotoPath = body.foto || ""; // Usar el valor del formulario si está presente
+  if (foto) {
+    const bytes = await foto.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const fileName = `${body.cedula}.png`;
+    const filePath = path.join(process.cwd(), "public/img/photo", fileName);
+    await writeFile(filePath, buffer);
+
+    fotoPath = `/img/photo/${fileName}`;
+  }
+
   // Upsert para PERSONA
   const persona = await prisma.pERSONA.upsert({
-    where: { id: body.id || 0 }, // Usar un valor que no existe para que se inserte si no se encuentra
+    where: { id: id || 0 }, // Utilizar 0 si id es null para la creación
     update: {
-      idCampusPertenece: body.idCampusPertenece,
-      idParroquiaPertenece: body.idParroquiaPertenece,
+      id: id, // Este campo generalmente no debe ser actualizado
       cedula: body.cedula,
       nombre: body.nombre,
       apellido: body.apellido,
-      fechaNacimiento: body.fechaNacimiento,
-      sexo: body.sexo,
-      telefono: body.telefono,
+      idCampusPertenece: parseInt(body.idCampusPertenece, 10),
+      idParroquiaPertenece: parseInt(body.idParroquiaPertenece, 10),
       direccion: body.direccion,
+      fechaNacimiento: new Date(body.fechaNacimiento),
       nacionalidad: body.nacionalidad,
-      foto: body.foto,
+      telefono: body.telefono,
+      sexo: body.sexo,
+      foto: fotoPath,
     },
     create: {
-      idCampusPertenece: body.idCampusPertenece,
-      idParroquiaPertenece: body.idParroquiaPertenece,
+      id: id, // Este campo generalmente no debe ser creado
       cedula: body.cedula,
       nombre: body.nombre,
       apellido: body.apellido,
-      fechaNacimiento: body.fechaNacimiento,
-      sexo: body.sexo,
-      telefono: body.telefono,
+      idCampusPertenece: parseInt(body.idCampusPertenece, 10),
+      idParroquiaPertenece: parseInt(body.idParroquiaPertenece, 10),
       direccion: body.direccion,
+      fechaNacimiento: new Date(body.fechaNacimiento),
       nacionalidad: body.nacionalidad,
-      foto: body.foto,
+      telefono: body.telefono,
+      sexo: body.sexo,
+      foto: fotoPath,
     },
   });
 
   // Upsert para DOCENTE
   const docente = await prisma.dOCENTE.upsert({
-    where: { idPersonaPertenece: body.id || 0 }, // Usar un valor que no existe para que se inserte si no se encuentra
+    where: { idPersonaPertenece: id || 0 },
     update: {
       PERSONA: {
-        connect: {
-          id: persona.id,
-        },
+        connect: { id: persona.id },
       },
-      tiempoExperiencia: body.tiempoExperiencia,
+      tiempoExperiencia: parseInt(body.tiempoExperiencia, 10),
     },
     create: {
       PERSONA: {
-        connect: {
-          id: persona.id,
-        },
+        connect: { id: persona.id },
       },
-      tiempoExperiencia: body.tiempoExperiencia,
+      tiempoExperiencia: parseInt(body.tiempoExperiencia, 10),
     },
   });
 
   // Upsert para USUARIO
   const usuario = await prisma.uSUARIO.upsert({
-    where: { idPersonaPertenece: body.id || 0 }, // Usar un valor que no existe para que se inserte si no se encuentra
+    where: { idPersonaPertenece: id || 0 },
     update: {
       correo: body.correo,
       contrasena: hashedPassword,
       PERSONA: {
-        connect: {
-          id: persona.id,
-        },
+        connect: { id: persona.id },
       },
     },
     create: {
       correo: body.correo,
       contrasena: hashedPassword,
       PERSONA: {
-        connect: {
-          id: persona.id,
-        },
+        connect: { id: persona.id },
       },
     },
   });
@@ -149,42 +136,34 @@ export async function POST(request, { params }) {
       where: { idUsuarioPertenece: usuario.id },
     });
 
-    for (const rol of body.roles) {
+    for (const rol of JSON.parse(body.roles)) {
       await prisma.dETALLEROL.create({
         data: {
           USUARIO: {
-            connect: {
-              id: usuario.id,
-            },
+            connect: { id: usuario.id },
           },
           ROL: {
-            connect: {
-              id: rol.id,
-            },
+            connect: { id: rol.id },
           },
         },
       });
     }
   }
 
-  // Crear o actualizar detalles de docente titulo
+  // Crear o actualizar detalles de docente título
   if (body.titulos) {
     await prisma.dETALLEDOCENTETITULO.deleteMany({
       where: { idDocentePertenece: docente.id },
     });
 
-    for (const titulo of body.titulos) {
+    for (const titulo of JSON.parse(body.titulos)) {
       await prisma.dETALLEDOCENTETITULO.create({
         data: {
           DOCENTE: {
-            connect: {
-              id: docente.id,
-            },
+            connect: { id: docente.id },
           },
           TITULO: {
-            connect: {
-              id: titulo.id,
-            },
+            connect: { id: titulo.id },
           },
         },
       });
@@ -197,18 +176,14 @@ export async function POST(request, { params }) {
       where: { idDocentePertenece: docente.id },
     });
 
-    for (const experiencia of body.experiencias) {
+    for (const experiencia of JSON.parse(body.experiencias)) {
       await prisma.dETALLEDOCENTEEXPERIENCIA.create({
         data: {
           DOCENTE: {
-            connect: {
-              id: docente.id,
-            },
+            connect: { id: docente.id },
           },
           EXPERIENCIA: {
-            connect: {
-              id: experiencia.id,
-            },
+            connect: { id: experiencia.id },
           },
           cargo: experiencia.cargo,
         },
