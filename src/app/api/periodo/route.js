778143b3a8c893917357dae6b2e62d06014d2187
Jose <jojeda5171@uta.edu.ec>
2024-08-27 10:24:19 -0500

    feat: Eliminar estado innecesario en la creación de periodo

--- src/app/api/periodo/route.js
+++ src/app/api/periodo/route.js
@@ -109,7 +109,6 @@ export async function POST(request) {
           fechaInicio: inicio,
           fechaFin: fin,
           descripcion: body.descripcion,
-          estado: true,
         },
         create: {
           idEvaluacionPertenece: evaluacion.id,
