efd9a0d8e296cc75f7ecf373566c0243c12557d8
Jose <jojeda5171@uta.edu.ec>
2024-11-21 20:29:25 -0500

    actualizar los datos en el perfil del estudiante

---
+++ src/app/estudiante/layout.jsx
@@ -0,0 +1,25 @@
+// admin/DocenteLayout.js
+"use client";
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
+import { useSession } from "next-auth/react";
+
+const EstudianteLayout = ({ children }) => {
+  const { data: session } = useSession();
+  const roles = session?.user?.roles || [];
+
+  return (
+    <div className="docente-layout">
+      <Header />
+      <div className="main-content">
+        {/* <Sidebar /> */}
+        <div className="page-content">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EstudianteLayout;
