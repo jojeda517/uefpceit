c7d1f62642f08e5299bb411b23b855d25648dfad
Jose <jojeda5171@uta.edu.ec>
2024-11-30 13:39:42 -0500

    get estadistica

---
+++ src/app/api/reportes/estadistico/[idCampus]/[idPeriodo]/route.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prisma";
+
+export async function GET(_, { params }) {
+  try {
+    const { idCampus, idPeriodo } = params;
+
+    if (!idCampus || !idPeriodo) {
+      return NextResponse.json(
+        { message: "Faltan parámetros obligatorios" },
+        { status: 400 }
+      );
+    }
+
+    const campusId = parseInt(idCampus);
+    const periodoId = parseInt(idPeriodo);
+  } catch (error) {
+    console.error("Error al generar el reporte:", error);
+    return NextResponse.json(
+      { message: "Error al obtener datos del reporte" },
+      { status: 500 }
+    );
+  }
+}
